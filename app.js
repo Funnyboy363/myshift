0dc8252280c807e9b93e6c69805fa5570eeba4be
funnyboy363 <grantshoop@gmail.com>
2020-04-20 13:32:08 -0600

    adjusted content

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const MongoDBStore = require('connect-mongodb-session')(session);
 const csrf = require('csurf');
 const flash = require('connect-flash');
 require('dotenv').config(); //this is what helps to hide the login for MongoDB connection
-const cors = require('cors');
-const { ApolloServer } = require('apollo-server-express');
+// const cors = require('cors');
+// const { ApolloServer } = require('apollo-server-express');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
